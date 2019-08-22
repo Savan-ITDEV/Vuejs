a37a6c4204f2801a1e22813341584a4c58d153b4
Savan-ITDEV <savanitdev@gmail.com>
2019-08-22 23:13:59 +0700

    b3

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDyBqpaDIs-61iXxmaNQCHWyGtonqncrYk",
+  authDomain: "vuejs-bfdd1.firebaseapp.com",
+  databaseURL: "https://vuejs-bfdd1.firebaseio.com",
+  projectId: "vuejs-bfdd1",
+  storageBucket: "vuejs-bfdd1.appspot.com",
+  messagingSenderId: "551786342837"
+};
